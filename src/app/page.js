9bf81dfd66b04a1cb768a21f3573fbd0c2bcab1f
DannerDQ <dannerdiazquispe2@gmail.com>
2024-01-03 18:26:12 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,86 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import "./page.css"
 
 export default function Home() {
+  const [orders, setOrders] = useState({});
+  const menu = [
+    {
+      name: "combinado ",
+      description: "broaster, papas, chaufa",
+      price: 11.0,
+    },
+    { name: "salchipapa", description: "papa, hotdog", price: 10.0 },
+    { name: "broaster", description: "papa, pollo", price: 10.0 },
+    { name: "mixto", description: "papa, hotdog,broaster", price: 11.0 },
+    {
+      name: "triple",
+      description: "chaufa, papa, huevo o hotdog, broaster",
+      price: 13.0,
+    },
+  ];
+
+  const addOrder = (order, id) => {
+    const _orders = {...orders}
+    _orders[id] = order;
+    setOrders(_orders);
+  };
+
+  const remOrder = (id) => {
+    const _orders = { ...orders };
+    delete _orders[id];
+    setOrders(_orders);
+  };
+
+  const renderOrders = () => {
+    const ordersToRender = [];
+    for (let id in orders) {
+      const { name, price } = orders[id];
+
+      ordersToRender.push(
+        <li key={id}>
+          <h3>
+            {name} - S/.{price}
+          </h3>
+          <button onClick={() => remOrder(id)} className="remove">X</button>
+        </li>
+      );
+    }
+
+    return ordersToRender;
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className="menu">
+        <h2>Carta:</h2>
+        <ul>
+          {menu.map((dish) => {
+            const id = crypto.randomUUID();
+
+            return (
+              <li key={id}>
+                <button onClick={() => addOrder(dish, id)} className="add">
+                  {dish.name} - S/.{dish.price}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section >
+        <h2>Pedidos:</h2>
+        <div className="summary">
+          <ul>
+            {Object.keys(orders).length === 0 ? (
+              <li className="empty_summary">Aún no hay pedidos</li>
+            ) : (
+              renderOrders()
+            )}
+          </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
